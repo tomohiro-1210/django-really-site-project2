71d27835c62261643013ab95c16bce799cca1237
tomohiro_0225 <express22212270@gmail.com>
2024-09-11 19:43:16 +0900

    記事一覧とページャー実装

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import path, include
-from .views import article
+from .views import article, blog_list
 
 urlpatterns = [
     path('article/<int:pk>', article, name="article"),
+    path('list', blog_list, name="blog_list"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Article
+from django.core.paginator import Paginator
 
 # Create your views here.
+# 記事詳細
 def article(request, pk):
+    # 記事の個別データ引っ張り出し
     article = Article.objects.get(pk=pk)
+    # テンプレートページなどの読み込み
     template = 'blog/article.html'
     context = {'article':article}
+    return render(request, template, context)
+
+# 記事一覧
+def blog_list(request):
+    # 記事データの読み込み
+    articles = Article.objects.all()
+    # ページャー
+    paginator = Paginator(articles, 2)
+    page_num = request.GET.get('page')
+    # テンプレートなどの設定
+    template = 'blog/blog_list.html'
+    context = {
+        'articles':articles,
+        'page_obj':paginator.get_page(page_num),
+        'page_num':page_num
+        }
     return render(request, template, context)
