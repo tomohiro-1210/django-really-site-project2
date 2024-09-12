261b0c2fc2a50189e9b7f62d44a68479297eb923
tomohiro_0225 <express22212270@gmail.com>
2024-09-12 10:32:06 +0900

    プロフィールページの作成

--- mysiteapp/views.py
+++ mysiteapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Article
+
+# マイページ
+def mypage(request):
+    template = "mysite/mypage.html"
+    context = {}
+    return render(request, template, context)
