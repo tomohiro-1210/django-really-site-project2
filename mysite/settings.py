351f30131c2d86cba7be9512fd8af5464aade8c8
tomohiro_0225 <express22212270@gmail.com>
2024-09-13 09:43:42 +0900

    staticファイルの設定

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,7 +132,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# ------------------static設定項目------------------
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+# ------------------static設定項目------------------
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
